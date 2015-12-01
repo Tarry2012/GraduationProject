1d42569158679cc4da5959acd0c098c14ec564f8
Tarry2012 <tangqyuan@gmail.com>
2015-12-01 21:09:00 +0800

    数据库表

---
+++ GraduationProject/wendao.sql
@@ -0,0 +1,201 @@
+-- MySQL dump 10.14  Distrib 5.5.43-MariaDB, for Linux (i686)
+--
+-- Host: localhost    Database: wendao
+-- ------------------------------------------------------
+-- Server version	5.5.43-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Comment`
+--
+
+DROP TABLE IF EXISTS `Comment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Comment` (
+  `comment_id` int(11) NOT NULL AUTO_INCREMENT,
+  `commented_id` int(11) DEFAULT NULL,
+  `user_id` int(11) NOT NULL,
+  `video_id` int(11) NOT NULL,
+  `comment_like` int(10) unsigned DEFAULT NULL,
+  `comment_content` varchar(1024) NOT NULL,
+  `comment_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `is_reply` int(11) NOT NULL,
+  PRIMARY KEY (`comment_id`),
+  KEY `user_id` (`user_id`),
+  KEY `video_id` (`video_id`),
+  CONSTRAINT `Comment_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `User` (`user_id`),
+  CONSTRAINT `Comment_ibfk_2` FOREIGN KEY (`video_id`) REFERENCES `Video` (`video_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Comment`
+--
+
+LOCK TABLES `Comment` WRITE;
+/*!40000 ALTER TABLE `Comment` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Comment` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Note`
+--
+
+DROP TABLE IF EXISTS `Note`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Note` (
+  `user_id` int(11) DEFAULT NULL,
+  `video_id` int(11) DEFAULT NULL,
+  `note_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `note_content` varchar(4096) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Note`
+--
+
+LOCK TABLES `Note` WRITE;
+/*!40000 ALTER TABLE `Note` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Note` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `User`
+--
+
+DROP TABLE IF EXISTS `User`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `User` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(20) NOT NULL,
+  `user_passwd` varchar(20) NOT NULL,
+  `user_sex` char(2) DEFAULT NULL,
+  `user_mail` varchar(50) NOT NULL,
+  `user_picture` varchar(100) DEFAULT NULL,
+  `user_home_page` varchar(100) DEFAULT NULL,
+  `user_interest_type` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `user_name` (`user_name`),
+  UNIQUE KEY `user_mail` (`user_mail`),
+  UNIQUE KEY `user_mail_2` (`user_mail`),
+  UNIQUE KEY `user_home_page` (`user_home_page`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `User`
+--
+
+LOCK TABLES `User` WRITE;
+/*!40000 ALTER TABLE `User` DISABLE KEYS */;
+INSERT INTO `User` VALUES (1,'test','test',NULL,'',NULL,NULL,NULL),(3,'test1','test2',NULL,'353216124@qq.com',NULL,NULL,NULL);
+/*!40000 ALTER TABLE `User` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `User_Video`
+--
+
+DROP TABLE IF EXISTS `User_Video`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `User_Video` (
+  `user_id` int(11) NOT NULL DEFAULT '0',
+  `video_id` int(11) NOT NULL DEFAULT '0',
+  `type_id` int(11) NOT NULL,
+  PRIMARY KEY (`user_id`,`video_id`),
+  KEY `video_id` (`video_id`),
+  KEY `type_id` (`type_id`),
+  CONSTRAINT `User_Video_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `User` (`user_id`),
+  CONSTRAINT `User_Video_ibfk_2` FOREIGN KEY (`video_id`) REFERENCES `Video` (`video_id`),
+  CONSTRAINT `User_Video_ibfk_3` FOREIGN KEY (`type_id`) REFERENCES `Video_Type` (`type_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `User_Video`
+--
+
+LOCK TABLES `User_Video` WRITE;
+/*!40000 ALTER TABLE `User_Video` DISABLE KEYS */;
+/*!40000 ALTER TABLE `User_Video` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Video`
+--
+
+DROP TABLE IF EXISTS `Video`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Video` (
+  `video_id` int(11) NOT NULL AUTO_INCREMENT,
+  `video_name` varchar(20) NOT NULL,
+  `video_describe` varchar(1024) DEFAULT NULL,
+  `video_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `video_url` varchar(100) DEFAULT NULL,
+  `video_comment` int(10) unsigned DEFAULT NULL,
+  `video_like` int(10) unsigned DEFAULT NULL,
+  `video_play` int(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (`video_id`),
+  UNIQUE KEY `video_name` (`video_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Video`
+--
+
+LOCK TABLES `Video` WRITE;
+/*!40000 ALTER TABLE `Video` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Video` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Video_Type`
+--
+
+DROP TABLE IF EXISTS `Video_Type`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Video_Type` (
+  `type_id` int(11) NOT NULL AUTO_INCREMENT,
+  `type_name` varchar(20) NOT NULL,
+  PRIMARY KEY (`type_id`),
+  UNIQUE KEY `type_name` (`type_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Video_Type`
+--
+
+LOCK TABLES `Video_Type` WRITE;
+/*!40000 ALTER TABLE `Video_Type` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Video_Type` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-12-01 21:04:43
